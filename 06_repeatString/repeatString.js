b6a5e65b04ab59122f041d34625467573bf83bc7
vshibe <novabladmon@gmail.com>
2025-08-02 23:10:10 +0100

    finished: excersize 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
+	let final_string = "";
+	
+	if (count < 0) {return "ERROR"};
 
+	for(let i = 0; i < count; i++) {
+		final_string += string;
+	};
+
+	return final_string;
 };
 
 // Do not edit below this line
