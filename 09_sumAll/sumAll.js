4df98af968280ebac6318b8b02005ad1c8808a90
vshibe <novabladmon@gmail.com>
2025-08-04 16:11:09 +0100

    finished: excersize 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+	// its probably inefficient to have these seperated instead of as a single call, but who cares for an excersize
+	const isNumber = args.every(arg => typeof(arg) === "number");
+	const isNegative = args.some(arg => arg < 0);	
+	const isInteger = args.every(arg => arg === Math.floor(arg)); // turns out Number.isInteger exists lol
+	args.sort();
 
+	if (!isNumber || isNegative || !isInteger) {
+		return "ERROR";
+	}
+	
+	let count = 0;
+
+	for(let i = args[0]; i <= args[args.length-1]; i++) {
+		count += i;
+	}
+
+	return count;
 };
 
 // Do not edit below this line
