c9629bc4176ab31abc81cf6e0e6a59a65a346177
vshibe <novabladmon@gmail.com>
2025-09-11 12:40:57 +0100

    finished: excersize 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+	const divFour = year % 4 === 0;
+	const divHundred = year % 100 === 0;
+	const divFourHundred = year % 400 === 0;
 
+	return (divFour && (!divHundred || divFourHundred));
 };
 
 // Do not edit below this line
