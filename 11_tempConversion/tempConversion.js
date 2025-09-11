f372e9ef90ae9922b1c1c78cd8e58f3d01b085c3
vshibe <novabladmon@gmail.com>
2025-09-11 12:49:15 +0100

    finished: excersize 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+	temp = (temp-32)*5/9;
+	return Math.round(temp*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+	temp = (temp*9/5)+32;
+	return Math.round(temp*10)/10;
 };
 
 // Do not edit below this line
