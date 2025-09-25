6557a77cce4ded29a9a9657a16213adc154837c9
vshibe <novabladmon@gmail.com>
2025-09-25 21:45:02 +0100

    finished: excersize 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+	arr.sort((a, b) => {
+		return getAge(a.yearOfBirth, a.yearOfDeath)-getAge(b.yearOfBirth, b.yearOfDeath);
+	});
+	return arr[arr.length-1];
+};
 
+const getAge = function(birth, death) {
+	if(!death) {
+		death = new Date().getFullYear();
+	};
+
+	return death-birth;
 };
 
 // Do not edit below this line
