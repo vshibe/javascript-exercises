a9442b739c6008ff62f3251baff8024567084c11
vshibe <novabladmon@gmail.com>
2025-09-25 20:23:52 +0100

    finished: excersize 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.length > 0 ? arr.reduce((accumulator, numB) => {return accumulator+numB}) : 0;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.length > 0 ? arr.reduce((accumulator, numB) => {return accumulator*numB}) : 0;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let res = 1;
+
+	for(let i = 2; i <= num; i++) {
+		res *= i;
+	};
+
+	return res;
 };
 
 // Do not edit below this line
