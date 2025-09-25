68ac0c416fb59c5a6bcba6636d273bdc9377ecd8
vshibe <novabladmon@gmail.com>
2025-09-25 20:48:53 +0100

    finished: excersize 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+	let fib = [0, 1, 1];
 
+	for(let i = 3; i <= num; i++) {
+		fib[i] = fib[i-1] + fib[i-2];
+	};
+
+	return fib[num];
 };
 
 // Do not edit below this line
